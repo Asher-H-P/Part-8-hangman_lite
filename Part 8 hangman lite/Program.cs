fc9109d9d7199afbcfb89cf1559de3f5aec8b043
Asher Heywood Peters <339513541@GoTVDSB.ca>
2023-11-15 11:01:00 -0500

    Did some.

--- Part 8 hangman lite/Program.cs
+++ Part 8 hangman lite/Program.cs
@@ -12,119 +12,37 @@ namespace Part_8_hangman_lite
         public static void Main(string[] args)
         {
             Random generator = new Random();
+            Console.WriteLine("Welcome to hangman lite.");
+            Console.WriteLine("Here are the rules:");
+            Console.WriteLine("1. Try to guess the letters in the mystery word.");
+            Console.WriteLine("2. Getting letters wrong results in another section of the man being hung.");
+            Console.WriteLine("3. Once the man is fully hung, you lose.");
+            Console.WriteLine("4. If you get all the letters right before the man dies, you win.");
+            Console.WriteLine("Good luck.");
             Word(generator.Next(1, 7));
-            //Use word3 to make a random number to choose word2 so you can put that into the answer List.
         }
         public static void Word(int word1)
         {
-            string word2;
-            string incrct = "";
-            string blank;
-            int wrong = 0;
-            int right = 0;
-            int word3;
+            string word = "COMPUTER";
+            string displayWord = "________";
+            string lettersGuessed = "";
+            int incorrect = 0;
             bool done = false;
             while (!done)
             {
-                Console.WriteLine(incrct);
-                Hung(wrong);
-                if (word1 == 1)
-                {
-                    word3 = 1;
-                    word2 = "I" + "T" + "A" + "L" + "Y";
-                    blank = ("_" + "_" + "_" + "_" + "_");
-                    Console.WriteLine(blank.Substring(0, 5));
-                }
-                else if (word1 == 2)
-                {
-                    word2 = "K" + "E" + "N" + "Y" + "A";
-                    blank = ("_" + "_" + "_" + "_" + "_" + "_" + "_");
-                    Console.WriteLine(blank.Substring(0, 7));
-                }
-                else if (word1 == 3)
-                {
-                    word2 = "B" + "R" + "A" + "Z" + "I" + "L";
-                    blank = ("_" + "_" + "_" + "_" + "_" + "_");
-                    Console.WriteLine(blank.Substring(0, 6));
-                }
-                else if (word1 == 4)
-                {
-                    word2 = "C" + "U" + "B" + "A";
-                    blank = ("_" + "_" + "_" + "_");
-                    Console.WriteLine(blank.Substring(0, 4));
-                }
-                else if (word1 == 5)
-                {
-                    word2 = "S" + "P" + "A" + "I" + "N";
-                    blank = ("_" + "_" + "_" + "_" + "_" + "_" + "_" + "_");
-                    Console.WriteLine(blank.Substring(0, 8));
-                }
-                else if (word1 == 7)
-                {
-                    word2 = "U" + "K" + "R" + "A" + "I" + "N" + "E";
-                    blank = ("_" + "_" + "_" + "_" + "_" + "_" + "_");
-                    Console.WriteLine(blank.Substring(0, 7));
-                }
-                else if (word1 == 6)
-                {
-                    word2 = "U" + "Z" + "B" + "E" + "K" + "I" + "S" + "T" + "A" + "N";
-                    blank = ("_" + "_" + "_" + "_" + "_" + "_" + "_" + "_" + "_" + "_");
-                    Console.WriteLine(blank.Substring(0, 10));
-                }
-                else if (word1 == 8)
-                {
-                    word2 = "G" + "U" + "I" + "N" + "E" + "A";
-                    blank = ("_" + "_" + "_" + "_" + "_" + "_");
-                    Console.WriteLine(blank.Substring(0, 6));
-                }
-                else if (word1 == 9)
-                {
-                    word2 = "P" + "O" + "R" + "T" + "U" + "G" + "A" + "L";
-                    blank = ("_" + "_" + "_" + "_" + "_" + "_" + "_" + "_");
-                    Console.WriteLine(blank.Substring(0, 8));
-                }
-                else if (word1 == 10)
-                {
-                    word2 = "N" + "O" + "R" + "W" + "A" + "Y";
-                    blank = ("_" + "_" + "_" + "_" + "_" + "_");
-                    Console.WriteLine(blank.Substring(0, 6));
-                }
-                else
-                {
-                    Console.WriteLine("ERROR!");
-                }
-                Console.Write("What letter would you like to guess? ");
+                Console.WriteLine(lettersGuessed);
+                Hung(incorrect);
+                Console.WriteLine(displayWord);
+                Console.WriteLine($"You have {3 - incorrect} guesses left");
+                Console.Write("Please input your guess: ");
                 string guess = Console.ReadLine().ToUpper();
-                List <string> answer = new List<string>();
-                if (answer.Contains(guess))
+                if (word.Contains(guess))
                 {
-                    Console.Beep(2000, 300);
-                    right = right + 1;
-                    if (right == answer.Count + 1)
-                    {
-                        done = true;
-                    }
+                    Console.WriteLine("Yep");
                 }
-                else if (!answer.Contains(guess))
+                else if (!word.Contains(guess))
                 {
-                    Console.Beep(1000, 300);
-                    Console.WriteLine("Incorrect guess");
-                    if (incrct.Contains(guess))
-                    {
-                        Console.WriteLine("You've already guessed this!");
-                    }
-                    else if (!incrct.Contains(guess))
-                    {
-                        incrct = incrct + guess;
-                        wrong = wrong + 1;
-                        if (wrong == 3)
-                        {
-                            Hung(wrong);
-                            Console.WriteLine("YOU LOSE!");
-                            Console.WriteLine();
-                            done = true;
-                        }
-                    }
+                    Console.WriteLine("Nope");
                 }
             }
         }
